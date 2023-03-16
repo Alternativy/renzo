cafb8f3a499fc33a4538b6e253c8ba965c2f0b0e
alternativy <alternativy4.0@gmail.com>
2023-03-16 07:21:34 -0600

    modificaciones visuales

--- scripts.js
+++ scripts.js
@@ -1,8 +1,35 @@
-const sectionLinks = document.querySelectorAll('nav a');
+// const sectionLinks = document.querySelectorAll('nav a');
 
-sectionLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        const targetSection = document.querySelector(link.getAttribute('href'));
-        targetSection.scrollIntoView({behavior: 'smooth'});
+// sectionLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         const targetSection = document.querySelector(link.getAttribute('href'));
+//         targetSection.scrollIntoView({behavior: 'smooth'});
+//     });
+// });
+
+
+// Obtener todos los enlaces de navegación
+const enlaces = document.querySelectorAll('nav a');
+
+// Recorrer los enlaces y agregar un evento "click" a cada uno
+enlaces.forEach((enlace) => {
+  enlace.addEventListener('click', (evento) => {
+    // Prevenir el comportamiento por defecto del enlace (scroll)
+    evento.preventDefault();
+    
+    // Obtener el ID de la sección correspondiente al enlace
+    const seccionId = enlace.getAttribute('href').substring(1);
+    
+    // Obtener todas las secciones
+    const secciones = document.querySelectorAll('main > div');
+    
+    // Recorrer las secciones y ocultar todas excepto la correspondiente al enlace
+    secciones.forEach((seccion) => {
+      if (seccion.getAttribute('id') === seccionId) {
+        seccion.classList.remove('oculto');
+      } else {
+        seccion.classList.add('oculto');
+      }
     });
+  });
 });
